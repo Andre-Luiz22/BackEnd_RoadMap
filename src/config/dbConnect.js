28effe5575ec7df5d58ab7dd6bce118d76664396
AndreLuiz2022 <andreluiz01bhz@gmail.com>
2023-11-02 18:04:36 -0300

    Commit Inicial

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_CONNECTION_STRING);
+mongoose.connect(
+    "mongodb+srv://admin:admin@cluster0.gssk2ux.mongodb.net/roadmapdb?retryWrites=true&w=majority"
+);
 
 let db = mongoose.connection;
 
